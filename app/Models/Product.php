747082487f4be85d89a6a88d7961c110d046c90d
Leonardo Aragão <lewoaragao@gmail.com>
2023-03-30 13:25:02 -0300

    organização model Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,6 +10,9 @@ class Product extends Model
     // use HasFactory;
 
     protected $fillable = [
-        'name', 'price', 'description', 'slug'
+        'name',
+        'price',
+        'description',
+        'slug'
     ];
 }
