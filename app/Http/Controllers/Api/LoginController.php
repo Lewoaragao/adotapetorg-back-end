e86c290ae1b40b5d9d1af07dffb53e5c29085eb7
Leonardo Aragão <lewoaragao@gmail.com>
2023-04-19 09:28:24 -0300

    Update LoginController.php

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -9,20 +9,8 @@ use App\Models\User;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 
-
-
 class LoginController extends Controller
 {
-    /**
-     * @var user
-     */
-    private $user;
-
-    public function __construct(User $user)
-    {
-        $this->user = $user;
-    }
-
     public function loginUser(Request $request): Response
     {
         $validator = Validator::make($request->all(), [
@@ -47,24 +35,24 @@ class LoginController extends Controller
         return Response(['message' => 'email or password wrong'], 401);
     }
 
-    // public function userDetails(): Response
-    // {
-    //     if (Auth::check()) {
+    public function userDetails(): Response
+    {
+        if (Auth::check()) {
 
-    //         $user = Auth::user();
+            $user = Auth::user();
 
-    //         return Response(['data' => $user], 200);
-    //     }
+            return Response(['data' => $user], 200);
+        }
 
-    //     return Response(['data' => 'Unauthorized'], 401);
-    // }
+        return Response(['data' => 'Unauthorized'], 401);
+    }
 
-    // public function logout(): Response
-    // {
-    //     $user = Auth::user();
+    public function logout(): Response
+    {
+        $user = Auth::user();
 
-    //     $user->currentAccessToken()->delete();
+        $user->currentAccessToken()->delete();
 
-    //     return Response(['data' => 'User Logout successfully.'], 200);
-    // }
+        return Response(['data' => 'User Logout successfully.'], 200);
+    }
 }
