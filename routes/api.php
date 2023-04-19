33a00a575c1d30ef2e5ba600e53b0ed21125e6a8
Leonardo Aragão <lewoaragao@gmail.com>
2023-04-19 08:16:19 -0300

    teste sanctum

--- routes/api.php
+++ routes/api.php
@@ -46,10 +46,10 @@ Route::namespace('App\Http\Controllers\Api')->group(function(){
         Route::delete('/{id}', 'PetController@destroy'); // DELETA UM PET
     });
 
-    Route::post('login', 'LoginController@loginUser');
-    Route::group(['middleware' => 'auth:sanctum'],function(){
-        Route::get('user', 'LoginController@userDetails');
-        Route::get('logout', 'LoginController@logout');
-    });
 });
 
+Route::post('login', 'App\Http\Controllers\Api\LoginController@loginUser');
+Route::group(['middleware' => 'auth:sanctum'],function(){
+    Route::get('user', 'LoginController@userDetails');
+    Route::get('logout', 'LoginController@logout');
+});
